7c823433dd43e8387b92288f8deefb5fb2af85e5
Pranav Vashi <neobuddy89@gmail.com>
2020-03-24 17:55:24 +0530

    DNM: msm8998-common: Update IMS stack
    * Remove CNEServiceApp
    * Start moving to /product
    * Add new packages

--- common.mk
+++ common.mk
@@ -382,10 +382,11 @@ PRODUCT_PACKAGES += \
     ims-ext-common \
     ims-ext-common_system \
     telephony-ext \
-		qti-telephony-hidl-wrapper \
-		qti_telephony_hidl_wrapper.xml \
-		qti-telephony-utils \
-		qti_telephony_utils.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+		ims_ext_common.xml \
 
 PRODUCT_BOOT_JARS += \
     ims-ext-common_system \
